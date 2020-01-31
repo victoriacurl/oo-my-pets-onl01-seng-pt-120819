5ba7e511dbd024f564ac5cbeee1ec1b41785c321
Victoria Curl <victoriacurl9@gmail.com>
2020-01-31 15:28:12 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,10 @@
 class Cat
+  # code goes here
   attr_accessor :mood
- attr_reader :name
+  attr_reader :name
 
- def initialize(name)
-   @name = name
-   @mood = "nervous"
- end
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
